3b354e665cfd5b9a8485a6aa3de313125af52bfd
Martin Pospisil <m.pospisil76@gmail.com>
2021-01-05 23:09:34 +0100

    adding mainviewmodel

---
+++ src/FridgeApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FridgeApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FridgeApp.ViewModels
+{
+	public interface IMainViewModel
+	{
+
+	}
+
+	public class MainViewModel : BaseViewModel, IMainViewModel
+	{
+		public MainViewModel(IFridgeDAL fridgeDal) : base(fridgeDal)
+		{
+
+		}
+
+
+	}
+}
