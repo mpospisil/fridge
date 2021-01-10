162047e2ea2cf14e721fbe08a01de83f6cdca729
Martin Pospisil <m.pospisil76@gmail.com>
2021-01-10 12:47:46 +0100

    implementation ItemViewModel

---
+++ src/FridgeApp/ViewModels/ItemViewModel.cs
@@ -0,0 +1,71 @@
+﻿using FridgeApp.Services;
+using System;
+
+namespace FridgeApp.ViewModels
+{
+	public interface IItemViewModel
+	{
+		Guid ItemId { get; }
+
+		string Name { get; }
+
+		string FridgeName { get; }
+
+		int FridgeIndex { get; }
+
+		string PartitionName { get; }
+
+		int PartitionIndex { get; }
+	}
+
+	public class ItemViewModel : BaseViewModel, IItemViewModel
+	{
+		string name;
+		string fridgeName;
+		int fridgeIndex;
+		string partitionName;
+		int partitionIndex;
+
+		public ItemViewModel(IFridgeDAL fridgeDal) : base(fridgeDal)
+		{
+		}
+
+		Guid itemId;
+
+		public Guid ItemId
+		{
+			get => itemId;
+			set => SetProperty(ref itemId, value);
+		}
+
+		public string Name
+		{
+			get => name;
+			set => SetProperty(ref name, value);
+		}
+
+		public string FridgeName
+		{
+			get => fridgeName;
+			set => SetProperty(ref fridgeName, value);
+		}
+
+		public int FridgeIndex
+		{
+			get => fridgeIndex;
+			set => SetProperty(ref fridgeIndex, value);
+		}
+
+		public string PartitionName
+		{
+			get => partitionName;
+			set => SetProperty(ref partitionName, value);
+		}
+
+		public int PartitionIndex
+		{
+			get => partitionIndex;
+			set => SetProperty(ref partitionIndex, value);
+		}
+	}
+}
--- src/FridgeApp/ViewModels/MainViewModel.cs
+++ src/FridgeApp/ViewModels/MainViewModel.cs
@@ -1,22 +1,30 @@
 ﻿using FridgeApp.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace FridgeApp.ViewModels
 {
 	public interface IMainViewModel
 	{
-
+		void OnAppearing();
+		ObservableCollection<ItemViewModel> Items { get; }
 	}
 
 	public class MainViewModel : BaseViewModel, IMainViewModel
 	{
+		
 		public MainViewModel(IFridgeDAL fridgeDal) : base(fridgeDal)
 		{
-
+			this.Items = new ObservableCollection<ItemViewModel>();
 		}
 
+		public ObservableCollection<ItemViewModel> Items { get; private set; }
 
+		public void OnAppearing()
+		{
+			IsBusy = true;
+		}
 	}
 }
