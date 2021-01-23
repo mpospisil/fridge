a729ef99d67b2bbe77579da8096bf83f24549dbe
Martin Pospisil <m.pospisil76@gmail.com>
2021-01-23 17:18:39 +0100

    mainpage - showing partitions

--- src/FridgeApp/ViewModels/MainViewModel.cs
+++ src/FridgeApp/ViewModels/MainViewModel.cs
@@ -10,7 +10,7 @@ namespace FridgeApp.ViewModels
 	public interface IMainViewModel
 	{
 		void OnAppearing();
-		ObservableCollection<ItemViewModel> Items { get; }
+		ObservableCollection<IItemViewModel> Items { get; }
 
 		Command LoadItemsCommand { get; }
 	}
@@ -20,13 +20,19 @@ namespace FridgeApp.ViewModels
 		
 		public MainViewModel(IFridgeDAL fridgeDal) : base(fridgeDal)
 		{
-			Items = new ObservableCollection<ItemViewModel>();
+			Items = new ObservableCollection<IItemViewModel>();
+			Fridges = new ObservableCollection<IFridgeViewModel>();
 			LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+			LoadFridgesCommand = new Command(async () => await ExecuteLoadFridges());
 		}
 
-		public ObservableCollection<ItemViewModel> Items { get; private set; }
+		public ObservableCollection<IItemViewModel> Items { get; private set; }
+
+		public ObservableCollection<IFridgeViewModel> Fridges { get; private set; }
 
 		public Command LoadItemsCommand { get; private set; }
+
+		public Command LoadFridgesCommand { get; private set; }
 
 		public void OnAppearing()
 		{
@@ -45,6 +51,30 @@ namespace FridgeApp.ViewModels
 				{
 					var itemVM = new ItemViewModel(FridgeDal, item);
 					Items.Add(itemVM);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+
+		async Task ExecuteLoadFridges()
+		{
+			IsBusy = true;
+
+			try
+			{
+				Fridges.Clear();
+				var fridges = await FridgeDal.GetFridgesAsync(true);
+				foreach (var fridge in fridges)
+				{
+					var fridgeVM = new FridgeViewModel(FridgeDal, fridge);
+					Fridges.Add(fridgeVM);
 				}
 			}
 			catch (Exception ex)
