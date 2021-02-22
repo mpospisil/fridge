9de24bebfcb1881c44b9d5a574e39ce29d8f226a
Martin Pospisil <m.pospisil76@gmail.com>
2021-02-22 23:43:25 +0100

    adding ItemsViewModel

--- src/FridgeApp/ViewModels/ItemsViewModel.cs
+++ src/FridgeApp/ViewModels/ItemsViewModel.cs
@@ -1,50 +1,33 @@
 ﻿using FridgeApp.Services;
-using FridgeApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using System.Linq;
 
 namespace FridgeApp.ViewModels
 {
-	public interface IMainViewModel
+	public interface IItemsViewModel
 	{
 		void OnAppearing();
 		ObservableCollection<IItemViewModel> Items { get; }
-
 		Command LoadItemsCommand { get; }
 	}
 
-	/// <summary>
-	/// The view model for the main screen
-	/// </summary>
-	public class MainViewModel : BaseViewModel, IMainViewModel
+	public class ItemsViewModel : BaseViewModel, IItemsViewModel
 	{
-		
-		public MainViewModel(IFridgeDAL fridgeDal) : base(fridgeDal)
+		public ItemsViewModel(IFridgeDAL fridgeDal) : base(fridgeDal)
 		{
 			Items = new ObservableCollection<IItemViewModel>();
-			Fridges = new ObservableCollection<IFridgeViewModel>();
 			LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
-			LoadFridgesCommand = new Command(async () => await ExecuteLoadFridges());
 		}
-
 		/// <summary>
 		/// All items for the user
 		/// </summary>
 		public ObservableCollection<IItemViewModel> Items { get; private set; }
 
-		/// <summary>
-		/// All fridges for the user
-		/// </summary>
-		public ObservableCollection<IFridgeViewModel> Fridges { get; private set; }
-
 		public Command LoadItemsCommand { get; private set; }
-
-		public Command LoadFridgesCommand { get; private set; }
 
 		public void OnAppearing()
 		{
@@ -65,12 +48,13 @@ namespace FridgeApp.ViewModels
 			Dictionary<Guid, PartitionDescriptor> partitionDescriptorDict = new Dictionary<Guid, PartitionDescriptor>();
 
 			int fridgeInx = 0;
-			foreach(var fridge in fridges)
+			foreach (var fridge in fridges)
 			{
 				int partitionInx = 0;
-				foreach(var partition in fridge.Partitions)
+				foreach (var partition in fridge.Partitions)
 				{
-					partitionDescriptorDict.Add(partition.PartitionId, new PartitionDescriptor() {
+					partitionDescriptorDict.Add(partition.PartitionId, new PartitionDescriptor()
+					{
 						FridgeInx = fridgeInx,
 						PartitionInx = partitionInx,
 						Fridge = fridge,
@@ -89,7 +73,7 @@ namespace FridgeApp.ViewModels
 				foreach (var item in items)
 				{
 					PartitionDescriptor partitionDescriptor = null;
-					if(!partitionDescriptorDict.TryGetValue(item.PartitionId, out partitionDescriptor))
+					if (!partitionDescriptorDict.TryGetValue(item.PartitionId, out partitionDescriptor))
 					{
 						// partition doesn't exist
 						continue;
@@ -102,34 +86,6 @@ namespace FridgeApp.ViewModels
 					itemVM.FridgeName = partitionDescriptor.Fridge.Name;
 					itemVM.PartitionName = partitionDescriptor.Partition.Name;
 					Items.Add(itemVM);
-				}
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex);
-			}
-			finally
-			{
-				IsBusy = false;
-			}
-		}
-
-		/// <summary>
-		/// Get all fridges and store them to the collection Fridges 
-		/// </summary>
-		/// <returns></returns>
-		async Task ExecuteLoadFridges()
-		{
-			IsBusy = true;
-
-			try
-			{
-				Fridges.Clear();
-				var fridges = await FridgeDal.GetFridgesAsync(true);
-				foreach (var fridge in fridges)
-				{
-					var fridgeVM = new FridgeViewModel(FridgeDal, fridge);
-					Fridges.Add(fridgeVM);
 				}
 			}
 			catch (Exception ex)
--- src/UT_FridgeApp/MainViewModelTest.cs
+++ src/UT_FridgeApp/MainViewModelTest.cs
@@ -2,7 +2,6 @@
 using FridgeApp.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
