3697d3780c562c7c904e81ea43ad0293b40b5f73
Martin Pospisil <m.pospisil76@gmail.com>
2021-03-14 22:49:28 +0100

    adding class FridgeForUser

---
+++ src/Fridge.Model/FridgeForUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fridge.Model
+{
+	/// <summary>
+	/// Information about user's fridge
+	/// </summary>
+	public class FridgeForUser
+	{
+		/// <summary>
+		/// The unique identifier of the fridge
+		/// </summary>
+		public Guid FridgeId { get; set; }
+
+
+	}
+}
