2cd43c630d6a8eeb07c7c02dac1ef1bf2193062c
Martin Pospisil <m.pospisil76@gmail.com>
2021-03-24 00:10:31 +0100

    adding UT for RepositoryLiteDb

--- src/Fridge.Repository/RepositoryLiteDb.cs
+++ src/Fridge.Repository/RepositoryLiteDb.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 
 namespace Fridge.Repository
 {
-	public class RepositoryLiteDb
+	public class RepositoryLiteDb : IDisposable
 	{
+		private bool disposedValue;
+		private readonly LiteDatabase Db;
+
 		public RepositoryLiteDb(string connectionString)
 		{
+			Db = new LiteDatabase(connectionString);
+		}
 
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					if(Db != null)
+					{
+						Db.Dispose();
+					}
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~RepositoryLiteDb()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
 		}
 	}
 }
--- src/UT_Fridge.Repository/RepositoryTest.cs
+++ src/UT_Fridge.Repository/RepositoryTest.cs
@@ -1,4 +1,6 @@
+using Fridge.Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace UT_Fridge.Repository
 {
@@ -8,6 +10,26 @@ namespace UT_Fridge.Repository
 		[TestMethod]
 		public void CreateRepositoryTest()
 		{
+			var tmpdir = System.IO.Path.GetTempPath();
+			var tempDbFileName = Path.Combine(tmpdir, "fridge.db");
+			try
+			{
+				using(var repos = new RepositoryLiteDb(tempDbFileName))
+				{
+
+				}
+			}
+			finally
+			{
+				try
+				{
+					if (File.Exists(tempDbFileName))
+					{
+						File.Delete(tempDbFileName);
+					}
+				}
+				catch { }
+			}
 		}
 	}
 }
