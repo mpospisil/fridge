01d9dfd0866e707045ce77796c53df19aeb84ee7
Martin Pospisil <m.pospisil76@gmail.com>
2021-03-24 07:47:01 +0100

    CreateUserAsync - implementation + test

--- src/Fridge.Repository/RepositoryLiteDb.cs
+++ src/Fridge.Repository/RepositoryLiteDb.cs
@@ -1,29 +1,114 @@
-﻿using FridgeApp.Services;
+﻿using Fridge.Model;
+using FridgeApp.Services;
 using LiteDB;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Fridge.Repository
 {
-	public class RepositoryLiteDb : IDisposable
+	public class RepositoryLiteDb : IFridgeDAL, IDisposable
 	{
+		// colection Names
+		static readonly string UserCollection = "users";
+
 		private bool disposedValue;
-		private readonly LiteDatabase Db;
+		private readonly LiteDatabase db;
 		private readonly IFridgeLogger Logger;
 
-		public RepositoryLiteDb(IFridgeLogger logger, string connectionString)
+		public RepositoryLiteDb(IFridgeLogger logger, string connectionString) 
 		{
 			this.Logger = logger;
 			Logger.LogDebug($"RepositoryLiteDb.RepositoryLiteDb connectionString = '{connectionString}'");
-			Db = new LiteDatabase(connectionString);
+			db = new LiteDatabase(connectionString);
 		}
 
+		public LiteDatabase Db => db;
+
+		#region implementation IFridgeDAL
+		public async Task<User> GetUserAsync()
+		{
+			return await Task.Run(() =>
+			{
+				Logger.LogDebug($"RepositoryLiteDb.GetUserAsync");
+				var users = Db.GetCollection<User>(UserCollection);
+
+				return users.FindAll().FirstOrDefault();
+			});
+		}
+
+		public async Task CreateUserAsync(User newUser)
+		{
+			await Task.Run(() =>
+			{
+				Logger.LogDebug($"RepositoryLiteDb.CreateUserAsync");
+				// Get a collection (or create, if doesn't exist)
+				var users = Db.GetCollection<User>(UserCollection);
+
+				var insertedVal = users.Insert(newUser);
+			});
+		}
+
+		public Task<IEnumerable<Model.Fridge>> GetFridgesAsync(bool forceRefresh = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Model.Fridge> GetFridgeAsync(Guid fridgeId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task AddFridge(Model.Fridge newFridgeData)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateFridge(Model.Fridge modifiedFridge)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteFridgeAsync(Guid fridgeId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<ItemInFridge>> GetItemsAsync(bool forceRefresh = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task AddItemAsync(ItemInFridge newFridgeData)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ItemInFridge> GetItemAsync(Guid itemId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateItemAsync(ItemInFridge modifiedItem)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteAsync(Guid itemInFridgeId)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		#region Disposing
 		protected virtual void Dispose(bool disposing)
 		{
 			if (!disposedValue)
 			{
 				if (disposing)
 				{
-					if(Db != null)
+					if (Db != null)
 					{
 						Db.Dispose();
 					}
@@ -48,5 +133,6 @@ namespace Fridge.Repository
 			Dispose(disposing: true);
 			GC.SuppressFinalize(this);
 		}
+		#endregion
 	}
 }
--- src/UT_Fridge.Repository/RepositoryTest.cs
+++ src/UT_Fridge.Repository/RepositoryTest.cs
@@ -1,25 +1,62 @@
+using Fridge.Model;
 using Fridge.Repository;
 using FridgeApp.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace UT_Fridge.Repository
 {
 	[TestClass]
 	public class RepositoryTest
 	{
+		static readonly Guid FirstUserId = Guid.Parse("58AEDAE6-11BB-41BB-9F9D-4576BE93915E");
+		static readonly string FirstUserName = "First User";
+		static readonly string FirstUserEmail = "name@comapany.com";
+
 		[TestMethod]
-		public void CreateRepositoryTest()
+		public async Task CreateRepositoryTest()
 		{
 			var fridgeLogger = Substitute.For<IFridgeLogger>();
 			var tmpdir = System.IO.Path.GetTempPath();
 			var tempDbFileName = Path.Combine(tmpdir, "fridge.db");
 			try
 			{
-				using(var repos = new RepositoryLiteDb(fridgeLogger, tempDbFileName))
+				using(var fridgeDal = new RepositoryLiteDb(fridgeLogger, tempDbFileName))
 				{
+					{
+						var firstUser = await fridgeDal.GetUserAsync();
+						Assert.IsNull(firstUser);
+					}
 
+					{
+						User user1 = new User()
+						{
+							UserId = FirstUserId,
+							Name = FirstUserName,
+							Email = FirstUserEmail
+						};
+
+						await fridgeDal.CreateUserAsync(user1);
+						var firstUser = await fridgeDal.GetUserAsync();
+						Assert.IsNotNull(firstUser);
+						Assert.IsTrue(firstUser.UserId == FirstUserId);
+						Assert.IsTrue(firstUser.Name == FirstUserName);
+						Assert.IsTrue(firstUser.Email == FirstUserEmail);
+					}
+				}
+
+				using (var fridgeDal = new RepositoryLiteDb(fridgeLogger, tempDbFileName))
+				{
+						{
+						var firstUser = await fridgeDal.GetUserAsync();
+						Assert.IsNotNull(firstUser);
+						Assert.IsTrue(firstUser.UserId == FirstUserId);
+						Assert.IsTrue(firstUser.Name == FirstUserName);
+						Assert.IsTrue(firstUser.Email == FirstUserEmail);
+					}
 				}
 			}
 			finally
