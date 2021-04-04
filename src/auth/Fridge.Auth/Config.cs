185cc14d91c63f5cebafff6b6bc8a4fd731eb4c5
Martin Pospisil <m.pospisil76@gmail.com>
2021-04-04 21:57:48 +0200

    adding IdentityServer4

---
+++ src/auth/Fridge.Auth/Config.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Fridge.Auth
+{
+	public static class Config
+	{
+		public static IEnumerable<IdentityResource> IdentityResources =>
+				new IdentityResource[]
+				{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile(),
+				};
+
+		public static IEnumerable<ApiScope> ApiScopes =>
+				new List<ApiScope>
+				{
+						new ApiScope("api1", "My API")
+				};
+
+		public static IEnumerable<Client> Clients =>
+				new List<Client>
+				{
+				new Client
+				{
+						ClientId = "client",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+						{
+								new Secret("secret".Sha256())
+						},
+
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+				},
+
+				new Client
+				{
+						ClientId = "mvc",
+						ClientSecrets = { new Secret("secret".Sha256()) },
+
+						AllowedGrantTypes = GrantTypes.Code,
+
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:5101/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5101/signout-callback-oidc" },
+
+						AllowedScopes = new List<string>
+						{
+								IdentityServerConstants.StandardScopes.OpenId,
+								IdentityServerConstants.StandardScopes.Profile
+						}
+				},
+
+				new Client
+				{
+					ClientId = "xamarin-client",
+					ClientName = "Xamarin Client",
+					AllowedGrantTypes = GrantTypes.Code,
+					AllowedScopes = new List<string>
+					{
+						IdentityServerConstants.StandardScopes.OpenId,
+						IdentityServerConstants.StandardScopes.Profile
+					},
+					AllowAccessTokensViaBrowser = true,
+					AllowOfflineAccess = true,
+					AlwaysIncludeUserClaimsInIdToken = true,
+					RequirePkce = true,
+					RequireClientSecret = false,
+					RedirectUris = { "https://localhost:5001/grands"},
+				},
+
+								new Client
+								{
+										ClientId = "interactive.public",
+										ClientName = "Interactive client (Code with PKCE)",
+
+										RedirectUris = { "https://notused" },
+										PostLogoutRedirectUris = { "https://notused" },
+
+										RequireClientSecret = false,
+
+										AllowedGrantTypes = GrantTypes.Code,
+										AllowedScopes = { "openid", "profile", "email"},
+
+										AllowOfflineAccess = true,
+										RefreshTokenUsage = TokenUsage.OneTimeOnly,
+										RefreshTokenExpiration = TokenExpiration.Sliding
+								},
+				};
+	}
+}
--- src/auth/Fridge.Auth/Startup.cs
+++ src/auth/Fridge.Auth/Startup.cs
@@ -2,56 +2,77 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Fridge.Auth
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
-            app.UseRouting();
+			var builder = services.AddIdentityServer(options =>
+			{
+				// see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+				options.EmitStaticAudienceClaim = true;
+			})
+					.AddInMemoryIdentityResources(Config.IdentityResources)
+					.AddInMemoryApiScopes(Config.ApiScopes)
+					.AddInMemoryClients(Config.Clients);
 
-            app.UseAuthorization();
+			// not recommended for production - you need to store your key material somewhere secure
+			builder.AddDeveloperSigningCredential();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			services.AddAuthentication()
+					.AddGoogle(options =>
+					{
+						options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+						// register your IdentityServer with Google at https://console.developers.google.com
+						// enable the Google+ API
+						// set the redirect URI to https://localhost:5001/signin-google
+						options.ClientId = "556744474248-2ovfttlln1uat9mmt05a4q45pj3ta4dq.apps.googleusercontent.com";
+						options.ClientSecret = "qinqSz_deP6_mY3E7IJy-7bu";
+					});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			// uncomment if you want to add MVC
+			app.UseStaticFiles();
+			app.UseRouting();
+
+			app.UseIdentityServer();
+
+			// uncomment, if you want to add MVC
+			app.UseAuthorization();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapDefaultControllerRoute();
+			});
+		}
+	}
 }
