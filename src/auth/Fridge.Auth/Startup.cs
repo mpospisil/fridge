a0fae051c3ba25b5b63280dd8870ea6cb764f64e
Martin Pospisil <m.pospisil76@gmail.com>
2021-04-04 18:09:44 +0200

    fridge.auth - empty lambda project

--- src/auth/Fridge.Auth/Startup.cs
+++ src/auth/Fridge.Auth/Startup.cs
@@ -1,73 +1,58 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using IdentityServer4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Fridge.Auth
 {
-	public class Startup
-	{
-		public IWebHostEnvironment Environment { get; }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public Startup(IWebHostEnvironment environment)
-		{
-			Environment = environment;
-		}
+        public IConfiguration Configuration { get; }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			// uncomment, if you want to add an MVC-based UI
-			services.AddControllersWithViews();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           services.AddRazorPages();
+        }
 
-			var builder = services.AddIdentityServer(options =>
-			{
-							// see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-							options.EmitStaticAudienceClaim = true;
-			})
-					.AddInMemoryIdentityResources(Config.IdentityResources)
-					.AddInMemoryApiScopes(Config.ApiScopes)
-					.AddInMemoryClients(Config.Clients);
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			// not recommended for production - you need to store your key material somewhere secure
-			builder.AddDeveloperSigningCredential();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			services.AddAuthentication()
-					.AddGoogle(options =>
-					{
-						options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            app.UseRouting();
 
-						// register your IdentityServer with Google at https://console.developers.google.com
-						// enable the Google+ API
-						// set the redirect URI to https://localhost:5001/signin-google
-						options.ClientId = "556744474248-2ovfttlln1uat9mmt05a4q45pj3ta4dq.apps.googleusercontent.com";
-						options.ClientSecret = "qinqSz_deP6_mY3E7IJy-7bu";
-					});
-		}
+            app.UseAuthorization();
 
-		public void Configure(IApplicationBuilder app)
-		{
-			if (Environment.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			// uncomment if you want to add MVC
-			app.UseStaticFiles();
-			app.UseRouting();
-
-			app.UseIdentityServer();
-
-			// uncomment, if you want to add MVC
-			app.UseAuthorization();
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapDefaultControllerRoute();
-			});
-		}
-	}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
 }
