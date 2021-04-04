6690c8b5debe913bd96ca2f91c1ce7420b3b5c1e
Martin Pospisil <m.pospisil76@gmail.com>
2021-04-04 18:45:39 +0200

    adding serilog

--- src/auth/Fridge.Auth/LocalEntryPoint.cs
+++ src/auth/Fridge.Auth/LocalEntryPoint.cs
@@ -1,23 +1,48 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 
 namespace Fridge.Auth
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+					.MinimumLevel.Debug()
+					.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+					.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+					.MinimumLevel.Override("System", LogEventLevel.Warning)
+					.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+					.Enrich.FromLogContext()
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+					.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+					.CreateLogger();
+
+			try
+			{
+				Log.Information("Starting host...");
+				CreateHostBuilder(args).Build().Run();
+
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Host terminated unexpectedly.");
+
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+				Host.CreateDefaultBuilder(args)
+						.ConfigureWebHostDefaults(webBuilder =>
+						{
+							webBuilder.UseStartup<Startup>();
+						});
+	}
 }
